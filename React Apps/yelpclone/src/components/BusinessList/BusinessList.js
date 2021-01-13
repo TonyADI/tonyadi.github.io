b919da33b797177f1ff2442db64ee54f09781cad
TonyADI <35707107+TonyADI@users.noreply.github.com>
2021-01-13 17:42:31 -0500

    New Project
    Making a clone of the yelp website.

---
+++ React Apps/yelpclone/src/components/BusinessList/BusinessList.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import { Business } from '../Business/Business';
+
+export class BusinessList extends React.Component{
+    render(){
+        return(
+            <div></div>
+        )
+    }
+}
