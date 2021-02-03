a0e96052adcda3fd69576269f42d7d15ac64a0bb
TonyADI <35707107+TonyADI@users.noreply.github.com>
2021-02-02 19:47:56 -0500

    initial commit for spotify playlist modifier

---
+++ React Apps/jamming/src/components/SearchBar/SearchBar.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import './SearchBar.css';
+
+export class SearchBar extends React.Component {
+    constructor(props){
+        super(props);
+        this.search = this.search.bind(this);
+        this.handleTermChange = this.handleTermChange.bind(this);
+    }
+
+    search(term){
+        this.props.onSearch(term);
+    }
+
+    handleTermChange(e){
+        this.search(e.target.value);
+    }
+
+    render(){
+        return (
+            <div className="SearchBar">
+                <input onChange={this.handleTermChange} placeholder="Enter a Song, Album, or Artist" />
+                <button className="SearchButton">SEARCH</button>
+            </div>
+        )
+    }
+}
