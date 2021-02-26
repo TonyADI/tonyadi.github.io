af91394e709fdfc6c94d2c0ec479b48b87110764
TonyADI <35707107+TonyADI@users.noreply.github.com>
2021-02-26 18:46:56 -0500

    removed a redundancy

--- React Apps/calculator/src/components/Result/Result.js
+++ React Apps/calculator/src/components/Result/Result.js
@@ -5,7 +5,7 @@ export class Result extends React.Component {
     render(){
         return (
             <div>
-                <input className="result" value={this.props.result} disabled={true}></input>
+                <input className="result" value={this.props.result} disabled></input>
             </div>
         );
     }
